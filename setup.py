7866043b940b339073a54022d124abdad641b4b2
Ilya Volodarsky <ivolo@mit.edu>
2012-10-16 22:32:28 -0700

    fixing setup long description

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
-long_description = ('Segment.io (https://segment.io) is a segmentation-focused'
-                    'analytics platform. If you haven\'t yet, register for a'
-                    'project at https://segment.io.'
+long_description = '''
+Segment.io (https://segment.io) is a segmentation-focused analytics platform.
+If you haven\'t yet, register for a project at https://segment.io.
 
-                    'This is the official python client that wraps the'
-                    '[Segment.io REST API](https://segment.io/docs)'
+This is the official python client that wraps the Segment.io REST API (https://segment.io/docs).
 
-                    'Documentation at https://github.com/segmentio/segment-python')
+Documentation and more details at https://github.com/segmentio/segment-python
+'''
 
 
 setup(
