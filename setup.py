9f7fe4b0f6d26c12d49aecba4f96d8ea4da8407b
Ilya Volodarsky <ivolo@mit.edu>
2012-10-16 18:30:31 -0700

    updating disttools

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from distutils.core import setup
 
+long_description = ('Segment.io (https://segment.io) is a segmentation-focused'
+                    'analytics platform. If you haven\'t yet, register for a'
+                    'project at https://segment.io.'
+
+                    'This is the official python client that wraps the'
+                    '[Segment.io REST API](https://segment.io/docs)'
+
+                    'Documentation at https://github.com/segmentio/segment-python')
+
+
 setup(
     name='segment',
     version='0.0.2',
@@ -14,5 +24,5 @@ setup(
         'requests'
     ],
     description='Official Segment.io Python Client',
-    long_description=open('README.md').read()
+    long_description=long_description
 )
