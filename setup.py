e90d918f1a2d475bee75daa635590cd25fffdec8
Ilya Volodarsky <ivolo@mit.edu>
2012-10-17 12:33:32 -0700

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Documentation and more details at https://github.com/segmentio/segment-python
 
 setup(
     name='segment',
-    version='0.0.5',
+    version='0.1.0',
     url='https://github.com/segmentio/segment-python',
     author='Ilya Volodarsky',
     author_email='ilya@segment.io',
