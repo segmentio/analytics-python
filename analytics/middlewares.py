63f71818aef42f30cbcac87824d1ead24e4477ba
Alan Justino <alan.justino@yahoo.com.br>
2013-05-01 00:45:02 -0300

    Decouplied implementation, using ContentModifier base
    AnalyticsJSInjector is based on reusable ContentModifier, that
    can inject or clear headers, and change even chunked responses
    without buffering it

--- analytics/middlewares.py
+++ analytics/middlewares.py
@@ -1,35 +1,106 @@
-#coding: utf-8
+# coding: utf-8
+# (c) 2013 Segment.io, Alan Justino da Silva and contributors;
+# Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 
-class AnalyticsJSInjector(object):
-    """Inject analytics.js snippet on <head>"""
-    def __init__(self, app, js_token):
+
+class ContentModifier(object):
+    """WSGI middleware that changes the content on the fly"""
+
+    ## Intended to be reimplemented by child classes:
+
+    def response_modifier(self, app_iter):
+        """Overwrite on children classes to change app_iter chunks on the fly
+
+        Clients might not bother calling .close() as it occurs by other means
+        """
+        for line in app_iter:
+            yield line
+
+    def header_modifier(self, status, headers, exc_info=None):
+        """Overwrite on children classes to change headers on the fly
+
+        It SHOULD return status, headers and exc_info, modified or not
+
+        Tip: Usually is not needed to chop 'Content-Length' as most servers
+        already do it if response_modifier() acts as an iterator
+        """
+        ## Usage example:
+        # headers.append(('X-Content-Modified', 'Yes'))
+        return status, headers, exc_info
+
+    ## No problem if reimplemented:
+
+    def __init__(self, app):
         self.app = app
-        self.js_token = js_token
+        super(ContentModifier, self).__init__()
 
-        self.js_snippet = u"""<script type="text/javascript">
-            //<![CDATA[
-            var analytics=analytics||[];analytics.load=function(e){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+e+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);var r=function(e){return function(){analytics.push([e].concat(Array.prototype.slice.call(arguments,0)))}},i=["identify","track","trackLink","trackForm","trackClick","trackSubmit","pageview","ab","alias","ready"];for(var s=0;s<i.length;s++)analytics[i[s]]=r(i[s])};
-            analytics.load("%(token)");
-            //]]>
-        </script>
-        """ % dict(token=self.js_token)
+    ## Not intended to be modified:
 
     def __call__(self, environ, start_response):
-        app_iter = self.app(environ, start_response)
+        app_iter = self.app(environ, self._start_response(start_response))
+        modified_iter = self.response_modifier(app_iter)
 
-        def injected_iter():
-            snippet_injected = False
-            for line in app_iter:
-                if not snippet_injected and u'var analytics=analytics||[];analytics.load=function(e)' in line.lower():
-                    # Detected already existing snippet. Stop trying to inject.
-                    snippet_injected = True
+        if hasattr(app_iter, 'close'):
+            modified_iter.close = lambda: app_iter.close()
+        return self._iter_closer(modified_iter)
 
-                if not snippet_injected and u'</head>' in line.lower(): # Add to the bottom of HEAD
-                    line.replace(u'</head>', self.js_snippet + u'</head>')
+    def _start_response(self, original_start_response):
+        def start_response(status, headers, exc_info=None):
+            status, headers, exc_info = self.header_modifier(status, headers, exc_info)
+            original_start_response(status, headers, exc_info)
+        return start_response
 
-                yield line
+    @staticmethod
+    def _iter_closer(modified_iter):
+        for line in modified_iter:
+            yield line
 
-            if hasattr(app_iter, 'close'):
-                app_iter.close()
+        if hasattr(modified_iter, 'close'):
+            modified_iter.close()
 
-        return injected_iter()
+
+class AnalyticsJSInjector(ContentModifier):
+    "Inject analytics.js snippet on <head/>, if analytics.js not exists there already"
+    def __init__(self, app, javascript_token):
+        self.js_token = javascript_token
+        self.js_snippet = """<script type="text/javascript">
+            //<![CDATA[
+            var analytics=analytics||[];analytics.load=function(e){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+e+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);var r=function(e){return function(){analytics.push([e].concat(Array.prototype.slice.call(arguments,0)))}},i=["identify","track","trackLink","trackForm","trackClick","trackSubmit","pageview","ab","alias","ready"];for(var s=0;s<i.length;s++)analytics[i[s]]=r(i[s])};
+            analytics.load("PLACEHOLDER_FOR_JSTOKEN");
+            //]]>
+        </script>
+        """.replace('PLACEHOLDER_FOR_JSTOKEN', self.js_token)
+        super(AnalyticsJSInjector, self).__init__(app)
+
+    def response_modifier(self, app_iter):
+        """Should be implemented by descendants"""
+        snippet_injected = False
+        for line in app_iter:
+            if not snippet_injected and u'var analytics=analytics||[];analytics.load=function(e)' in line.lower():
+                # Detected already existing snippet. Stop trying to inject another.
+                snippet_injected = True
+
+            if not snippet_injected and '</head>' in line.lower(): # Add to the bottom of HEAD
+                line = line.replace('</head>', self.js_snippet + '</head>')
+
+            yield line
+
+    def header_modifier(self, status, headers, exc_info=None):
+        # Suppress content-length, as this middleware can change response length
+        # and the server can calculate it on their own, by PEP 333 (WSGI)
+        headers = filter(lambda header: header[0].lower() != 'content-length', headers)
+        return status, headers, exc_info
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+
+    def sample_application(environ, start_response):
+        start_response("200 OK", [("Content-type", "text/plain")])
+        return ["<html><head><title>Hello World!</title></head></html>\n",]
+
+    app = sample_application
+    app = AnalyticsJSInjector(app, 'DEADBEEF')
+
+
+    server = make_server('localhost', 8080, app)
+    server.serve_forever()
