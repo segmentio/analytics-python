a832c6b3ba27e78dded0421f67f78bf21f66f4ce
Alan Justino <alan.justino@yahoo.com.br>
2013-06-09 13:06:43 -0300

    Safeguard against whiteline strippers

--- analytics/middlewares.py
+++ analytics/middlewares.py
@@ -64,10 +64,10 @@ class AnalyticsJSInjector(ContentModifier):
     def __init__(self, app, javascript_token):
         self.js_token = javascript_token
         self.js_snippet = """<script type="text/javascript">
-            //<![CDATA[
+            /* <![CDATA[ */
             var analytics=analytics||[];analytics.load=function(e){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+e+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);var r=function(e){return function(){analytics.push([e].concat(Array.prototype.slice.call(arguments,0)))}},i=["identify","track","trackLink","trackForm","trackClick","trackSubmit","pageview","ab","alias","ready"];for(var s=0;s<i.length;s++)analytics[i[s]]=r(i[s])};
             analytics.load("PLACEHOLDER_FOR_JSTOKEN");
-            //]]>
+            /* ]]> */
         </script>
         """.replace('PLACEHOLDER_FOR_JSTOKEN', self.js_token)
         super(AnalyticsJSInjector, self).__init__(app)
