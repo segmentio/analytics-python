4e9833513ea83db482da3a64dbda9e221d83733a
Ilya Volodarsky <ivolo@mit.edu>
2013-11-21 15:45:57 -0800

    fixing total_seconds python 2.6 compat issue and bump version

--- analytics/utils.py
+++ analytics/utils.py
@@ -7,6 +7,10 @@ def is_naive(dt):
     """ Determines if a given datetime.datetime is naive. """
     return dt.tzinfo is None or dt.tzinfo.utcoffset(dt) is None
 
+def total_seconds(delta):
+    """ Determines total seconds with python < 2.7 compat """
+    # http://stackoverflow.com/questions/3694835/python-2-6-5-divide-timedelta-with-timedelta
+    return (delta.microseconds + (delta.seconds + delta.days * 24 * 3600) * 1e6) / 1e6
 
 def guess_timezone(dt):
     """ Attempts to convert a naive datetime to an aware datetime """
@@ -15,7 +19,7 @@ def guess_timezone(dt):
         # case, and then defaults to utc
 
         delta = datetime.now() - dt
-        if delta.total_seconds() < 5:
+        if total_seconds(delta) < 5:
             # this was created using datetime.datetime.now()
             # so we are in the local timezone
             return dt.replace(tzinfo=tzlocal())
--- history.md
+++ history.md
@@ -1,8 +1,12 @@
-0.4.3 / 2013-11-13
+0.4.4 / November 21, 2013
+------------------
+* add < python 2.7 compatibility by removing `delta.total_seconds`
+
+0.4.3 / November 13, 2013
 ------------------
 * added datetime serialization fix (alexlouden)
 
-0.4.2 / 2013-06-25
+0.4.2 / June 26, 2013
 ------------------
 * Added history.d change log
 * Merging https://github.com/segmentio/analytics-python/pull/14 to add support for lists and PEP8 fixes. Thanks https://github.com/dfee!
