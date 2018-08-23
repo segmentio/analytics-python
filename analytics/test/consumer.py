c17fbb5fc606010848d2c62b891be407389f7fba
Nhi Nguyen <nhi.nguyen@segment.com>
2018-08-23 15:57:15 -0700

    Add unit tests for retry logic

--- analytics/test/consumer.py
+++ analytics/test/consumer.py
@@ -1,4 +1,5 @@
 import unittest
+import mock
 
 try:
     from queue import Queue
@@ -6,6 +7,7 @@ except:
     from Queue import Queue
 
 from analytics.consumer import Consumer
+from analytics.request import APIError
 
 
 class TestConsumer(unittest.TestCase):
@@ -46,6 +48,50 @@ class TestConsumer(unittest.TestCase):
             'userId': 'userId'
         }
         consumer.request([track])
+
+    def _test_request_retry(self, expected_exception, exception_count):
+
+        def mock_post(*args, **kwargs):
+            mock_post.call_count += 1
+            if mock_post.call_count <= exception_count:
+                raise expected_exception
+        mock_post.call_count = 0
+
+        with mock.patch('analytics.consumer.post', mock.Mock(side_effect=mock_post)):
+            consumer = Consumer(None, 'testsecret')
+            track = {
+                'type': 'track',
+                'event': 'python event',
+                'userId': 'userId'
+            }
+            # request() should succeed if the number of exceptions raised is less
+            # than the retries paramater.
+            if exception_count <= consumer.retries:
+                consumer.request([track])
+            else:
+                # if exceptions are raised more times than the retries parameter,
+                # we expect the exception to be returned to the caller.
+                with self.assertRaises(type(expected_exception)) as exc:
+                    consumer.request([track])
+                self.assertEqual(exc.exception, expected_exception)
+
+    def test_request_retry(self):
+        # we should retry on general errors
+        self._test_request_retry(Exception('generic exception'), 2)
+
+        # we should retry on server errors
+        self._test_request_retry(APIError(500, 'code', 'Internal Server Error'), 2)
+
+        # we should retry on HTTP 429 errors
+        self._test_request_retry(APIError(429, 'code', 'Too Many Requests'), 2)
+
+        # we should NOT retry on other client errors
+        api_error = APIError(400, 'code', 'Client Errors')
+        with self.assertRaises(APIError) as exc:
+            self._test_request_retry(api_error, 1)
+
+        # test for number of exceptions raise > retries value
+        self._test_request_retry(APIError(500, 'code', 'Internal Server Error'), 4)
 
     def test_pause(self):
         consumer = Consumer(None, 'testsecret')
