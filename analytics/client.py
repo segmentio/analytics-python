16e266109f6af7032826093fa36844100557a189
Pablo Berganza <pberganza@applaudostudios.com>
2019-12-11 17:01:06 -0600

    Add configurable number of threads to run consumer

--- analytics/client.py
+++ analytics/client.py
@@ -28,7 +28,7 @@ class Client(object):
     def __init__(self, write_key=None, host=None, debug=False,
                  max_queue_size=10000, send=True, on_error=None, flush_at=100,
                  flush_interval=0.5, gzip=False, max_retries=3,
-                 sync_mode=False, timeout=15):
+                 sync_mode=False, timeout=15, thread=1):
         require('write_key', write_key, string_types)
 
         self.queue = queue.Queue(max_queue_size)
@@ -45,21 +45,25 @@ class Client(object):
             self.log.setLevel(logging.DEBUG)
 
         if sync_mode:
-            self.consumer = None
+            self.consumers = None
         else:
-            self.consumer = Consumer(self.queue, write_key, host=host, on_error=on_error,
-                                     flush_at=flush_at, flush_interval=flush_interval,
-                                     gzip=gzip, retries=max_retries, timeout=timeout)
-
-            # if we've disabled sending, just don't start the consumer
+            # On program exit, allow the consumer thread to exit cleanly.
+            # This prevents exceptions and a messy shutdown when the interpreter is
+            # destroyed before the daemon thread finishes execution. However, it
+            # is *not* the same as flushing the queue! To guarantee all messages
+            # have been delivered, you'll still need to call flush().
             if send:
-                # On program exit, allow the consumer thread to exit cleanly.
-                # This prevents exceptions and a messy shutdown when the interpreter is
-                # destroyed before the daemon thread finishes execution. However, it
-                # is *not* the same as flushing the queue! To guarantee all messages
-                # have been delivered, you'll still need to call flush().
                 atexit.register(self.join)
-                self.consumer.start()
+            for n in range(thread):
+                self.consumers = []
+                consumer = Consumer(self.queue, write_key, host=host, on_error=on_error,
+                                    flush_at=flush_at, flush_interval=flush_interval,
+                                    gzip=gzip, retries=max_retries, timeout=timeout)
+                self.consumers.append(consumer)
+
+                # if we've disabled sending, just don't start the consumer
+                if send:
+                    consumer.start()
 
     def identify(self, user_id=None, traits=None, context=None, timestamp=None,
                  anonymous_id=None, integrations=None, message_id=None):
@@ -263,12 +267,13 @@ class Client(object):
 
     def join(self):
         """Ends the consumer thread once the queue is empty. Blocks execution until finished"""
-        self.consumer.pause()
-        try:
-            self.consumer.join()
-        except RuntimeError:
-            # consumer thread has not started
-            pass
+        for consumer in self.consumers:
+            consumer.pause()
+            try:
+                consumer.join()
+            except RuntimeError:
+                # consumer thread has not started
+                pass
 
     def shutdown(self):
         """Flush all messages and cleanly shutdown the client"""
