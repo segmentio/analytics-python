b7c7e401a33433c947e019c2cc2afae4d2659734
Pablo Berganza <pberganza@applaudostudios.com>
2019-12-19 14:59:20 -0600

    Fix linting errors on __init__.py

--- analytics/__init__.py
+++ analytics/__init__.py
@@ -19,29 +19,36 @@ def track(*args, **kwargs):
     """Send a track call."""
     _proxy('track', *args, **kwargs)
 
+
 def identify(*args, **kwargs):
     """Send a identify call."""
     _proxy('identify', *args, **kwargs)
+
 
 def group(*args, **kwargs):
     """Send a group call."""
     _proxy('group', *args, **kwargs)
 
+
 def alias(*args, **kwargs):
     """Send a alias call."""
     _proxy('alias', *args, **kwargs)
+
 
 def page(*args, **kwargs):
     """Send a page call."""
     _proxy('page', *args, **kwargs)
 
+
 def screen(*args, **kwargs):
     """Send a screen call."""
     _proxy('screen', *args, **kwargs)
 
+
 def flush():
     """Tell the client to flush."""
     _proxy('flush')
+
 
 def join():
     """Block program until the client clears the queue"""
@@ -58,8 +65,9 @@ def _proxy(method, *args, **kwargs):
     """Create an analytics client if one doesn't exist and send to it."""
     global default_client
     if not default_client:
-        default_client = Client(write_key, host=host, debug=debug, on_error=on_error,
-                                send=send, sync_mode=sync_mode)
+        default_client = Client(write_key, host=host, debug=debug,
+                                on_error=on_error, send=send,
+                                sync_mode=sync_mode)
 
     fn = getattr(default_client, method)
     fn(*args, **kwargs)
