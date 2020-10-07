fbd3c9283bce3d3c961617a377cf207c36d39fcd
Alon Diamant <alon@mayple.com>
2020-10-07 14:12:32 +0300

    Upgrade backoff dependency, drop Python 2.6 support

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ install_requires = [
     "requests>=2.7,<3.0",
     "six>=1.5",
     "monotonic>=1.5",
-    "backoff==1.6.0",
+    "backoff==1.10.0",
     "python-dateutil>2.1"
 ]
 
@@ -60,7 +60,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
