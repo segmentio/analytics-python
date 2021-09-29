010a238b7c8453c20352444b103bc65bb2ccbca3
Shane Duvall <sduvall@mark4tech.com>
2021-09-29 13:30:45 -0500

    Issue #154

--- segment/analytics/__init__.py
+++ segment/analytics/__init__.py
@@ -1,6 +1,6 @@
 
-from analytics.version import VERSION
-from analytics.client import Client
+from segment.analytics.version import VERSION
+from segment.analytics.client import Client
 
 __version__ = VERSION
 
