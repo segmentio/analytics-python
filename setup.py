677a31229bc1e8259c153f26dd09716c5ddf8da7
Shane Duvall <sduvall@mark4tech.com>
2021-10-04 10:57:01 -0500

    updating setup config

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ except ImportError:
     from distutils.core import setup
 
 # Don't import analytics-python module here, since deps may not be installed
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'segment'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'analytics'))
 from segment.analytics.version import VERSION
 
