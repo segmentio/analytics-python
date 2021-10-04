9ca0a4a5f8cc5e89ea2bd7afabf8e4d1bffa3cb3
Shane Duvall <sduvall@mark4tech.com>
2021-10-04 13:49:15 -0500

    namespace update

--- segment/analytics/__init__.py
+++ segment/analytics/__init__.py
@@ -1,6 +1,6 @@
 
-from segment.analytics.version import VERSION
-from segment.analytics.client import Client
+from analytics.version import VERSION
+from analytics.client import Client
 
 __version__ = VERSION
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 # Don't import analytics-python module here, since deps may not be installed
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'segment'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'analytics'))
-from segment.analytics.version import VERSION
+from analytics.version import VERSION
 
 long_description = '''
 Segment is the simplest way to integrate analytics into your application.
