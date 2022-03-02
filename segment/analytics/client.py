424a736721481941ee73148fbf72189eb3c7d89d
Shane L. Duvall <sduvall@mark4tech.com>
2022-03-02 13:26:43 -0600

    Update segment/analytics/client.py
    Co-authored-by: Patrick K <patkennel@gmail.com>

--- segment/analytics/client.py
+++ segment/analytics/client.py
@@ -276,7 +276,7 @@ class Client(object):
         # Check message size.
         msg_size = len(json.dumps(msg, cls=DatetimeSerializer).encode())
         if msg_size > MAX_MSG_SIZE:
-            raise RuntimeError('Message exceeds 32kb limit. (%s)', str(msg))
+            raise RuntimeError('Message exceeds %skb limit. (%s)', str(int(MAX_MSG_SIZE / 1024)), str(msg))
 
         # if send is False, return msg as if it was successfully queued
         if not self.send:
