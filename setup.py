dae446cb97e104ec85399a148c85e775a8b52f7a
Samantha Hughes <shughes.uk@gmail.com>
2022-06-20 14:54:17 -0700

    remove comment

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ except ImportError:
     from distutils.core import setup
 # Don't import analytics-python module here, since deps may not be installed
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'segment','analytics'))
-# sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'analytics'))
 from version import VERSION
 
 long_description = '''
