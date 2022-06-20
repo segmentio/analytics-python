0fc04d101a22f81441dc4faba8519e5d81fbc25d
Samantha Hughes <shughes.uk@gmail.com>
2022-06-20 14:47:28 -0700

    fix setup

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
 # Don't import analytics-python module here, since deps may not be installed
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'segment'))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'analytics'))
-from analytics.version import VERSION
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'segment','analytics'))
+# sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'analytics'))
+from version import VERSION
 
 long_description = '''
 Segment is the simplest way to integrate analytics into your application.
